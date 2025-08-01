67101ddb99a9a2398c8a45ac46b4709e0dba87b2
Hdbygdla <131492426+MiguelMoran1234@users.noreply.github.com>
2025-08-01 13:30:39 -0600

    Se creo el menu principal

--- Lab2P3_MiguelMoran.cpp
+++ Lab2P3_MiguelMoran.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    cout << "Hello World!\n";
+int TimeTrials() {
+    
+    return 0;
+}
+int Sumatoria() {
+    
+    return 0;
+}
+int main() {
+    int opcion;
+    do {
+        cout << "------- Menu -------" << endl;
+        cout << "1. Time trials" << endl;
+        cout << "2. Sumatoria" << endl;
+        cout << "Ingrese una opcion: ";
+        cin >> opcion;
+        switch (opcion) {
+
+        }
+    } while (opcion != 3);
 }
